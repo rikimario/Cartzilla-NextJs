a465e436758587eb0e976e373bfd2e2957b0bc41
rikimario <mario_martinov@abv.bg>
2024-06-16 22:39:46 +0300

    adding search bar for desktop view

--- src/components/navbar/DDMobileNav.tsx
+++ src/components/navbar/DDMobileNav.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { ChevronDown, ChevronUp, Home, User } from "lucide-react";
+import Link from "next/link";
 import React, { useState } from "react";
-export default function DDMobileNav() {
+
+type DDMobileNavProps = {
+  handleClickNav: (e: React.MouseEvent<HTMLButtonElement>) => void;
+};
+export default function DDMobileNav({ handleClickNav }: DDMobileNavProps) {
   const [openCategories, setOpenCategories] = useState<boolean>(false);
 
   const handleClickCategories = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -15,13 +20,17 @@ export default function DDMobileNav() {
           <li className="py-4 my-4 border-b border-[#E0E5EB]">
             <div className="flex gap-1 items-center justify-center pr-5">
               <Home className="h-5 w-5 dark:text-gray-400" />
-              <button type="button">Home</button>
+              <button onClick={handleClickNav} type="button">
+                <Link href="/">Home</Link>
+              </button>
             </div>
           </li>
           <li className="py-4 my-4 border-b border-[#E0E5EB]">
             <div className="flex gap-1 items-center justify-center pr-5">
               <User className="h-5 w-5 dark:text-gray-400" />
-              <button type="button">Profile</button>
+              <button onClick={handleClickNav} type="button">
+                <Link href="/profile">Profile</Link>
+              </button>
             </div>
           </li>
           <li className="py-4 my-4 border-b border-[#E0E5EB]">
@@ -38,32 +47,62 @@ export default function DDMobileNav() {
             <>
               {openCategories ? (
                 <div className="flex dark:text-light gap-1 items-center justify-center pt-4 md:pt-0">
-                  <ul className="p-4 md:p-8 text-lg text-center items-center text-[#333D4C] border-x border-[#E0E5EB]">
-                    <li className="py-4 border-b dark:text-light text-[#4f5e75] border-[#E0E5EB]">
-                      Clothes
-                    </li>
-                    <li className="py-4 border-b dark:text-light text-[#4f5e75] border-[#E0E5EB]">
-                      Shoes
-                    </li>
-                    <li className="py-4 border-b dark:text-light text-[#4f5e75] border-[#E0E5EB]">
-                      Electronics
-                    </li>
-                    <li className="py-4 border-b dark:text-light text-[#4f5e75] border-[#E0E5EB]">
-                      Furniture
-                    </li>
-                    <li className="py-4 border-b dark:text-light text-[#4f5e75] border-[#E0E5EB]">
-                      Miscellaneous
-                    </li>
+                  <ul className="p-4 gap-8 flex flex-col md:p-8 text-lg text-center items-center text-[#333D4C] border-x border-[#E0E5EB]">
+                    <button onClick={handleClickNav}>
+                      <Link
+                        href="/clothes"
+                        className="py-4 border-b dark:text-light text-[#4f5e75] border-[#E0E5EB]"
+                        type="button"
+                      >
+                        Clothes
+                      </Link>
+                    </button>
+                    <button onClick={handleClickNav}>
+                      <Link
+                        href="/shoes"
+                        className="py-4 border-b dark:text-light text-[#4f5e75] border-[#E0E5EB]"
+                      >
+                        Shoes
+                      </Link>
+                    </button>
+                    <button onClick={handleClickNav}>
+                      <Link
+                        href="/electronics"
+                        className="py-4 border-b dark:text-light text-[#4f5e75] border-[#E0E5EB]"
+                      >
+                        Electronics
+                      </Link>
+                    </button>
+                    <button onClick={handleClickNav}>
+                      <Link
+                        href="/furniture"
+                        className="py-4 border-b dark:text-light text-[#4f5e75] border-[#E0E5EB]"
+                      >
+                        Furniture
+                      </Link>
+                    </button>
+                    <button onClick={handleClickNav}>
+                      <Link
+                        href="/miscellaneous"
+                        className="py-4 border-b dark:text-light text-[#4f5e75] border-[#E0E5EB]"
+                      >
+                        Miscellaneous
+                      </Link>
+                    </button>
                   </ul>
                 </div>
               ) : null}
             </>
           </li>
           <li className="py-4 my-4 border-b border-[#E0E5EB]">
-            <button type="button">About</button>
+            <button onClick={handleClickNav} type="button">
+              <Link href="/about">About</Link>
+            </button>
           </li>
           <li className="py-4 my-4 border-b border-[#E0E5EB]">
-            <button type="button">Contact</button>
+            <button onClick={handleClickNav} type="button">
+              <Link href="/contact">Contact</Link>
+            </button>
           </li>
         </ul>
       </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Menu, ShoppingCart, X } from "lucide-react";
+import { Menu, ShoppingCart, User, X } from "lucide-react";
 import React, { useState } from "react";
 
 import Theme from "../Theme";
@@ -16,24 +16,48 @@ export default function Navbar() {
 
   return (
     <nav>
-      <div className="h-20 px-4 md:px-12 flex items-center bg-[#222934]">
-        <div className="flex-1 flex gap-2 items-center">
+      <div className="h-20 px-4 md:px-12 lg:px-0 flex items-center justify-between lg:justify-around bg-[#222934]">
+        <div className="flex gap-2 items-center">
           <button onClick={handleClickNav} className="lg:hidden text-[#E0E5EB]">
             {openNav ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
           </button>
-          <h1 className="text-white font-semibold text-xl leading-loose">
-            Cartzilla
-          </h1>
+          <div>
+            <div></div>
+            <h1 className="text-white font-semibold text-xl leading-loose">
+              Cartzilla
+            </h1>
+          </div>
         </div>
+
+        {/* Search  */}
+        <div className="hidden lg:flex gap-8">
+          <input
+            className="h-10 max-w-96 text-lg overflow-auto bg-[#333e4e] outline-none border-none text-[#E0E5EB] rounded-full pl-4 pr-12"
+            type="text"
+            placeholder="Search"
+          />
+          <div className="flex gap-2 items-center">
+            <div className="bg-[#333D4C] p-3 h-10 w-10 rounded-full flex items-center">
+              <span className="text-[#F55266]">%</span>
+            </div>
+            <div>
+              <p className="text-gray-400 text-xs">Only this month</p>
+              <h2 className="text-[#E0E5EB]">Super sale 20%</h2>
+            </div>
+          </div>
+        </div>
+
         <div className="flex gap-4 text-[#E0E5EB] items-center">
           <MobileSearch />
           <Theme />
+          <User className="h-5 w-5 hidden lg:block" strokeWidth={1} />
           <div className="bg-[#333D4C] p-3 rounded-full">
             <ShoppingCart className="h-5 w-5" strokeWidth={1} />
           </div>
         </div>
       </div>
-      {openNav && <DDMobileNav />}
+
+      {openNav && <DDMobileNav handleClickNav={handleClickNav} />}
     </nav>
   );
 }

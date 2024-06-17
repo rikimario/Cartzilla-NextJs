540b8cdf3a39e2a65f85ccae070301d2c4fcf2c5
rikimario <mario_martinov@abv.bg>
2024-06-17 23:58:27 +0300

    add desktop nav

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,22 @@
 "use client";
-import { Menu, ShoppingCart, User, X } from "lucide-react";
+import {
+  Menu,
+  ShoppingCart,
+  User,
+  X,
+  ChevronDown,
+  ChevronUp,
+} from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+import Link from "next/link";
 import React, { useState } from "react";
 
 import Theme from "../Theme";
@@ -8,10 +25,16 @@ import MobileSearch from "./MobileSearch";
 
 export default function Navbar() {
   const [openNav, setOpenNav] = useState<boolean>(false);
+  const [openCategories, setOpenCategories] = useState<boolean>(false);
 
   const handleClickNav = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setOpenNav(!openNav);
+  };
+
+  const handleClickCategories = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setOpenCategories(!openCategories);
   };
 
   return (
@@ -21,8 +44,10 @@ export default function Navbar() {
           <button onClick={handleClickNav} className="lg:hidden text-[#E0E5EB]">
             {openNav ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
           </button>
-          <div>
-            <div></div>
+          <div className="flex gap-2 items-center">
+            <div className="hidden lg:block">
+              <img src="logo.svg" alt="" />
+            </div>
             <h1 className="text-white font-semibold text-xl leading-loose">
               Cartzilla
             </h1>
@@ -54,6 +79,60 @@ export default function Navbar() {
           <div className="bg-[#333D4C] p-3 rounded-full">
             <ShoppingCart className="h-5 w-5" strokeWidth={1} />
           </div>
+        </div>
+      </div>
+
+      <div className="hidden font-light lg:flex px-4 md:px-12 lg:px-0 justify-around bg-[#222934]">
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            onClick={handleClickCategories}
+            className="text-[#E0E5EB] bg-gray-700 p-2 rounded-md flex gap-10"
+          >
+            Categories{" "}
+            <span>
+              {openCategories ? (
+                <ChevronUp strokeWidth={1} />
+              ) : (
+                <ChevronDown strokeWidth={1} />
+              )}
+            </span>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>Clothes</DropdownMenuItem>
+            <DropdownMenuItem>Shoes</DropdownMenuItem>
+            <DropdownMenuItem>Electronics</DropdownMenuItem>
+            <DropdownMenuItem>Miscellaneous</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <ul className="flex text-[#E0E5EB] gap-6">
+          <li className="hover:bg-gray-700 p-2 rounded-md">Best Sellers</li>
+          <li className="hover:bg-gray-700 p-2 rounded-md">Today's Deals</li>
+          <li className="hover:bg-gray-700 p-2 rounded-md">New Arrivals</li>
+          <li className="hover:bg-gray-700 p-2 rounded-md">About</li>
+          <li className="hover:bg-gray-700 p-2 rounded-md">Contact</li>
+        </ul>
+
+        <div className="flex text-[#E0E5EB] gap-6 p-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="text-[#E0E5EB]">
+              Eng
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Bg</DropdownMenuLabel>
+              <DropdownMenuLabel>Eng</DropdownMenuLabel>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger className="text-[#E0E5EB]">
+              USD($)
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>USD($)</DropdownMenuLabel>
+              <DropdownMenuLabel>BG(lv)</DropdownMenuLabel>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
 

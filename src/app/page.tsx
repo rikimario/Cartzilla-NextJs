57acaefb7407c7c95d522d22068396a5f383600f
rikimario <mario_martinov@abv.bg>
2024-06-25 23:54:55 +0300

    add home images and text for mobile and desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
+import { heroImages } from "../components/HeroSliderImgs";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ArrowUpRight } from "lucide-react";
 
 export default function Home() {
   const plugin = React.useRef(
@@ -18,17 +22,40 @@ export default function Home() {
     <div className="dark:bg-[#181D25] items-center justify-center flex">
       <Carousel
         plugins={[plugin.current]}
-        className="w-full p-4 md:px-12 lg:px-[7.6rem]"
-        onMouseEnter={plugin.current.stop}
-        onMouseLeave={plugin.current.reset}
+        opts={{ loop: true }}
+        className="w-full p-4 md:px-12 lg:px-[5rem] xl:px-[7.5rem]"
       >
         <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
+          {heroImages.map((hero) => (
             <CarouselItem>
-              <div className="">
+              <div>
                 <Card className="bg-gradient-to-r from-[#ACCBEE] to-[#E7F0FD] dark:bg-gradient-to-r dark:from-[#1B273A] dark:to-[#1F2632]">
-                  <CardContent className="flex min-h-[30rem] items-center justify-center">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  <CardContent className="flex flex-col lg:flex-row min-h-[30rem] text-center items-center justify-between lg:justify-center lg:text-start lg:gap-12 pt-10">
+                    <div className="lg:pl-24">
+                      <p className="text-gray-600 dark:text-gray-300 text-sm lg:text-base">
+                        {hero.p}
+                      </p>
+                      <h1 className="text-3xl lg:text-4xl font-semibold text-gray-900 dark:text-white pt-4">
+                        {hero.title}
+                      </h1>
+                      <div className="pt-4 lg:pt-8 flex gap-4 items-center justify-center lg:justify-start">
+                        <Button className="bg-[#F55266] hover:bg-[#F2223B] flex gap-1 items-center justify-center font-normal dark:text-white">
+                          Shop now{" "}
+                          <span>
+                            <ArrowUpRight className="h-5 w-5" strokeWidth={1} />
+                          </span>
+                        </Button>
+                      </div>
+                    </div>
+                    <div>
+                      <Image
+                        className="lg:w-[450px] lg:h-[450px]"
+                        src={hero.img}
+                        alt=""
+                        width={300}
+                        height={300}
+                      />
+                    </div>
                   </CardContent>
                 </Card>
               </div>
---
+++ src/components/HeroSliderImgs.tsx
@@ -0,0 +1,17 @@
+export const heroImages = [
+  {
+    p: "Feel the real quality sound",
+    title: "Headphones ProMax",
+    img: "/headphones-promax.png",
+  },
+  {
+    p: "Deal of the week",
+    title: "Powerful iPad Pro M2",
+    img: "/ipad-pro-2max.png",
+  },
+  {
+    p: "Virtual reality glasses",
+    title: "Experience new reality",
+    img: "/vr-glasses.png",
+  },
+];

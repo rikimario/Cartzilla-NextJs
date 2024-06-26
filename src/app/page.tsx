51e0ab85febb0bd2f9b234a206c19481b186dfd4
rikimario <mario_martinov@abv.bg>
2024-06-26 22:42:59 +0300

    add benefits section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Benefits from "@/components/benefits/Benefits";
 import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Benefits />
     </>
   );
 }
---
+++ src/components/benefits/Benefits.tsx
@@ -0,0 +1,63 @@
+import { CreditCard, MessageCircle, RefreshCcw, Truck } from "lucide-react";
+
+export default function Benefits() {
+  return (
+    <div className="dark:bg-[#181D25] p-4 lg:px-[4rem] xl:px-[7.5rem] lg:py-8 grid grid-cols-2 lg:grid-cols-4 gap-8 md:gap-4 lg:gap-2 items-center justify-center text-center">
+      <div className="flex flex-col lg:flex-row gap-2 items-center lg:justify-center">
+        <div className="bg-gray-100 p-6 rounded-full dark:bg-gray-800">
+          <Truck className="h-6 w-6 dark:text-white" />
+        </div>
+        <div className="lg:flex-col">
+          <p className="text-gray-900 dark:text-white font-semibold">
+            Free Shipping & Returns
+          </p>
+          <p className="text-sm text-gray-600 dark:text-gray-400">
+            For all orders over 199.00$
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row  gap-2 items-center lg:justify-center">
+        <div className="bg-gray-100 p-6 rounded-full dark:bg-gray-800">
+          <CreditCard className="h-6 w-6 dark:text-white" />
+        </div>
+        <div className="lg:flex-col">
+          <p className="text-gray-900 font-semibold dark:text-white">
+            Secure Payment Method
+          </p>
+          <p className="text-sm text-gray-600 dark:text-gray-400">
+            We ensure secure payment
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row  gap-2 items-center lg:justify-center">
+        <div className="bg-gray-100 p-6 rounded-full dark:bg-gray-800">
+          <RefreshCcw className="h-6 w-6 dark:text-white" />
+        </div>
+        <div className="lg:flex-col">
+          <p className="text-gray-900 font-semibold dark:text-white">
+            Money Back Guarantee
+          </p>
+          <p className="text-sm text-gray-600 dark:text-gray-400">
+            30 day money back guarantee
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row  gap-2 items-center lg:justify-center">
+        <div className="bg-gray-100 p-6 rounded-full dark:bg-gray-800">
+          <MessageCircle className="h-6 w-6 dark:text-white" />
+        </div>
+        <div className="lg:flex-col">
+          <p className="text-gray-900 font-semibold dark:text-white">
+            24/7 Customer Support
+          </p>
+          <p className="text-sm text-gray-600 dark:text-gray-400">
+            Friendly customer support
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}

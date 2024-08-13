0140acd04d6340cf3cbaee5e5ad6ff3fb95d9a4d
rikimario <mario_martinov@abv.bg>
2024-08-13 23:18:07 +0300

    add men's category h1 and sorting option

--- src/app/categories/all-men/page.tsx
+++ src/app/categories/all-men/page.tsx
@@ -1,5 +1,41 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import React from "react";
 
 export default function page() {
-  return <div>Men</div>;
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold">Men's Category</h1>
+
+      <div className="flex items-center gap-2 mt-4">
+        <label
+          className="text-sm font-semibold text-gray-900 dark:text-white"
+          htmlFor=""
+        >
+          Sort by:
+        </label>
+        <Select>
+          <SelectTrigger className="w-1/2 md:w-1/4 dark:bg-inherit text-gray-600 dark:text-white border-none">
+            <SelectValue className="text-gray-300" placeholder="Relevance" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="Relevance">Relevance</SelectItem>
+            <SelectItem value="Popularity">Popularity</SelectItem>
+            <SelectItem value="Price: Low to High">
+              Price: Low to High
+            </SelectItem>
+            <SelectItem value="Price: High to Low">
+              Price: High to Low
+            </SelectItem>
+            <SelectItem value="Newest Arrivals">Newest Arrivals</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+  );
 }

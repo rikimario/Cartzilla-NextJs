1a6d047e9e93ad427057dedadb73cc45d734e841
rikimario <mario_martinov@abv.bg>
2024-09-09 23:22:18 +0300

    add more width to skeleton div

--- src/app/utils/ProductSkeleton.tsx
+++ src/app/utils/ProductSkeleton.tsx
@@ -6,7 +6,7 @@ export default function ProductSkeleton() {
     <div className="relative flex flex-col text-center items-center gap-2 lg:min-w-[250px] pb-8 animate-pulse">
       <span className="hidden">Loading...</span>
 
-      <Skeleton className="md:w-[160px] md:h-[160px] lg:w-[200px] lg:h-[200px]" />
+      <Skeleton className="md:w-[160px] md:h-[160px] lg:w-[200px] lg:h-[200px] xl:w-[250px] xl:h-[250px]" />
       <div className="flex flex-col gap-2  w-full text-start p-2">
         <Skeleton className="h-4 w-full rounded" />
         <div className="flex items-center justify-between pt-4">

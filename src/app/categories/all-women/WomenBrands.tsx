cc077294e08f656ebfd2f64284a5332a16469590
rikimario <mario_martinov@abv.bg>
2024-11-05 22:24:54 +0200

    add women category

---
+++ src/app/categories/all-women/WomenBrands.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { getProductsWomen } from "@/app/utils/products";
+
+interface FilterBtnBrandsProps {
+  handleBrandClick: (brand: string) => void;
+  selectedBrands: string[];
+}
+
+export default function WomenBrands({
+  handleBrandClick,
+  selectedBrands,
+}: FilterBtnBrandsProps) {
+  const products = getProductsWomen();
+
+  return (
+    <div className="p-6 border border-gray-200 rounded-xl text-gray-700 dark:text-white mt-6">
+      <h2 className="text-start text-xl text-gray-700 dark:text-white font-semibold">
+        Brand
+      </h2>
+      <div className="relative flex flex-col items-start gap-2 mt-4">
+        {Array.from(new Set(products.map((item) => item.brand))).map(
+          (brand) => (
+            <button
+              key={brand}
+              onClick={() => handleBrandClick(brand)}
+              className="flex items-center space-x-2"
+            >
+              <Checkbox
+                checked={selectedBrands.includes(brand)}
+                id={brand}
+                key={brand}
+              />
+              <Label htmlFor={brand}>{brand}</Label>
+            </button>
+          )
+        )}
+      </div>
+    </div>
+  );
+}
--- src/app/categories/all-women/page.tsx
+++ src/app/categories/all-women/page.tsx
@@ -1,5 +1,181 @@
-import React from "react";
+"use client";
+import WomenProducts from "./WomenProducts";
+import SortProductsBtn from "../../utils/SortingUtils/SortProductsBtn";
+import { getProductsWomen, Product } from "@/app/utils/products";
+import { useEffect, useState } from "react";
+import FilterBtnCategories from "../../utils/SortingUtils/FilterBtnCategories";
+import FilterBtnPrice from "../../utils/SortingUtils/FilterBtnPrice";
+import WomenBrands from "./WomenBrands";
+import FilterButton from "../../utils/SortingUtils/FilterButton";
+
+enum SortOrder {
+  Relevance = "relevance",
+  Name = "name",
+  Low = "low",
+  High = "high",
+}
 
 export default function page() {
-  return <div>Women</div>;
+  const products = getProductsWomen();
+
+  const [sort, setSort] = useState(products);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
+  const [sortOrder, setSortOrder] = useState<SortOrder>(SortOrder.Relevance);
+  const [brands, setBrands] = useState<string[]>([]);
+  const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
+  const [value, setValue] = useState<[number, number]>([0, 14000]);
+  const getSortedProducts = (
+    filteredProducts: Product[],
+    sortOrder: SortOrder
+  ) => {
+    switch (sortOrder) {
+      case SortOrder.Relevance:
+        return filteredProducts.sort((a, b) => a.id - b.id);
+      case SortOrder.Name:
+        return filteredProducts.sort((a, b) => a.title.localeCompare(b.title));
+      case SortOrder.Low:
+        return filteredProducts.sort((a, b) => a.price - b.price);
+      case SortOrder.High:
+        return filteredProducts.sort((a, b) => b.price - a.price);
+      default:
+        return filteredProducts;
+    }
+  };
+
+  useEffect(() => {
+    setSort(products);
+  }, [products]);
+
+  useEffect(() => {
+    setSelectedCategories(categories);
+  }, [categories]);
+
+  useEffect(() => {
+    setSelectedBrands(brands);
+  }, [brands]);
+
+  useEffect(() => {
+    if (selectedCategories.length === 0 && selectedBrands.length === 0) {
+      setSort(products);
+    } else {
+      const filteredProducts = products.filter(
+        (product) =>
+          (selectedCategories.length === 0 ||
+            selectedCategories.includes(product.category)) &&
+          (selectedBrands.length === 0 ||
+            selectedBrands.includes(product.brand?.toString() || ""))
+      );
+      setSort(filteredProducts);
+    }
+  }, [selectedCategories, selectedBrands]);
+
+  const handleChange = (value: string) => {
+    if (value === "low") {
+      setSort((prev) => [...prev].sort((a, b) => a.price - b.price));
+      setSortOrder(SortOrder.Low);
+    } else if (value === "high") {
+      setSort((prev) => [...prev].sort((a, b) => b.price - a.price));
+      setSortOrder(SortOrder.High);
+    } else if (value === "name") {
+      setSort((prev) =>
+        [...prev].sort((a, b) => a.title.localeCompare(b.title))
+      );
+      setSortOrder(SortOrder.Name);
+    } else if (value === "relevance") {
+      setSort((prev) => [...prev].sort((a, b) => a.id - b.id));
+      setSortOrder(SortOrder.Relevance);
+    }
+  };
+
+  const handleCategoryClick = (category: string) => {
+    const isSelected = categories.includes(category);
+
+    if (isSelected) {
+      setCategories((prev) => prev.filter((c) => c !== category));
+    } else {
+      setCategories((prev) => [...prev, category]);
+    }
+
+    setSelectedCategories(categories);
+
+    const sortedProducts = getSortedProducts(products, sortOrder);
+    const filteredProducts = sortedProducts.filter((product) =>
+      categories.includes(product.category)
+    );
+    setSort(filteredProducts);
+  };
+
+  const handleBrandClick = (brand: string) => {
+    const isSelected = brands.includes(brand);
+
+    if (isSelected) {
+      setBrands((prev) => prev.filter((c) => c !== brand));
+      setSelectedBrands((prev) => prev.filter((c) => c !== brand));
+    } else {
+      setBrands((prev) => [...prev, brand]);
+      setSelectedBrands((prev) => [...prev, brand]);
+    }
+
+    const sortedProducts = getSortedProducts(products, sortOrder);
+    const filteredProducts = sortedProducts.filter((product) =>
+      selectedBrands.includes(product.brand?.toString() || "")
+    );
+    setSort(filteredProducts);
+  };
+
+  const handlePriceChange = (values: [number, number]) => {
+    const filteredProducts = products.filter(
+      (product) =>
+        (selectedCategories.length === 0 ||
+          selectedCategories.includes(product.category)) &&
+        (selectedBrands.length === 0 ||
+          selectedBrands.includes(product.brand?.toString() || "")) &&
+        product.price >= values[0] &&
+        product.price <= values[1]
+    );
+    setSort(filteredProducts);
+  };
+
+  return (
+    <div className="p-4 xl:px-[5.4rem] 2xl:px-[7.7rem] dark:bg-[#181D25]">
+      <h1 className="text-2xl font-semibold mt-10">Women</h1>
+
+      <SortProductsBtn handleChange={handleChange} />
+
+      <div className="flex lg:gap-4 md:justify-start w-full">
+        <div className="lg:block hidden">
+          <FilterBtnCategories
+            handleCategoryClick={handleCategoryClick}
+            selectedCategories={selectedCategories}
+            currentCategory="Women"
+          />
+          <FilterBtnPrice
+            values={value}
+            setValue={setValue}
+            onChange={handlePriceChange}
+          />
+          <WomenBrands
+            handleBrandClick={handleBrandClick}
+            selectedBrands={selectedBrands}
+          />
+        </div>
+        <div className="md:flex-1">
+          <WomenProducts products={sort} />
+        </div>
+        <div className="lg:hidden">
+          <FilterButton
+            handleCategoryClick={handleCategoryClick}
+            selectedCategories={selectedCategories}
+            values={value}
+            setValue={setValue}
+            onChange={handlePriceChange}
+            handleBrandClick={handleBrandClick}
+            selectedBrands={selectedBrands}
+            currentCategory="Women"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }

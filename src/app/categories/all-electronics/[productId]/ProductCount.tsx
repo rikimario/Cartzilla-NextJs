045b56f9688d20259f8b0d93261fb7e2de147ef0
rikimario <mario_martinov@abv.bg>
2024-11-05 22:21:00 +0200

    add electronics category

---
+++ src/app/categories/all-electronics/[productId]/ProductCount.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { Card } from "@/components/ui/card";
+import { Minus, Plus } from "lucide-react";
+import React, { useState } from "react";
+
+export default function ProductCount() {
+  const [count, setCount] = useState<number>(1);
+  return (
+    <div className="flex gap-4 text-xl font-medium">
+      <Card className="flex items-center gap-8 p-4">
+        <button
+          className="disabled:opacity-40"
+          onClick={() => setCount(count - 1)}
+          disabled={count <= 1}
+        >
+          <Minus />
+        </button>
+        <p className="w-8 text-center">{count}</p>
+        <button className="" onClick={() => setCount(count + 1)}>
+          <Plus />
+        </button>
+      </Card>
+    </div>
+  );
+}

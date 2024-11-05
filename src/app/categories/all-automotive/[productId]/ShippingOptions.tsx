6751ce782078dd1ac1300b8e0ad34f37007efc63
rikimario <mario_martinov@abv.bg>
2024-11-05 22:20:08 +0200

    add automotive category

---
+++ src/app/categories/all-automotive/[productId]/ShippingOptions.tsx
@@ -0,0 +1,78 @@
+import { Product } from "@/app/utils/products";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import React from "react";
+
+interface ShippingOptionsProps {
+  product: Product;
+}
+
+export default function ShippingOptions({ product }: ShippingOptionsProps) {
+  return (
+    <div className="mt-6">
+      <Accordion type="single" collapsible className="w-full">
+        {/* Shipping */}
+        <AccordionItem value="item-1">
+          <AccordionTrigger className="text-base font-semibold">
+            Shipping options
+          </AccordionTrigger>
+          <div className="flex justify-between">
+            <AccordionContent className="text-gray-600 dark:text-gray-300">
+              <div className="flex flex-col gap-5">
+                <p className="">Pickup from the store</p>
+                <p className="">Pickup from postal offices</p>
+                <p className="">Delivery by courier</p>
+              </div>
+            </AccordionContent>
+            <AccordionContent className="text-gray-600 dark:text-gray-300">
+              <div className="flex flex-col gap-5">
+                <p>Today</p>
+                <p>Tomorrow</p>
+                <p>2-3 days</p>
+              </div>
+            </AccordionContent>
+            <AccordionContent className="text-gray-600 dark:text-gray-300">
+              <div className="text-end flex flex-col gap-5">
+                <p className="text-gray-900 dark:text-gray-300 font-semibold">
+                  Free
+                </p>
+                <p className="text-gray-900 dark:text-gray-300 font-semibold">
+                  $5.00
+                </p>
+                <p className="text-gray-900 dark:text-gray-300 font-semibold">
+                  $10.00
+                </p>
+              </div>
+            </AccordionContent>
+          </div>
+        </AccordionItem>
+        {/* Warranty */}
+        <AccordionItem value="item-2">
+          <AccordionTrigger className="text-base font-semibold">
+            Warranty information
+          </AccordionTrigger>
+          <AccordionContent className="text-gray-600 dark:text-gray-300">
+            <p className="text-gray-600 dark:text-gray-300 font-semibold">
+              {product.warrantyInformation}
+            </p>
+          </AccordionContent>
+        </AccordionItem>
+        {/* Return */}
+        <AccordionItem value="item-3">
+          <AccordionTrigger className="text-base font-semibold">
+            Return policy
+          </AccordionTrigger>
+          <AccordionContent className="text-gray-600 dark:text-gray-300">
+            <p className="text-gray-600 dark:text-gray-300 font-semibold">
+              {product.returnPolicy}
+            </p>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
--- src/app/categories/all-automotive/page.tsx
+++ src/app/categories/all-automotive/page.tsx
@@ -1,5 +1,182 @@
-import React from "react";
+"use client";
+import SortProductsBtn from "../../utils/SortingUtils/SortProductsBtn";
+import { getProductsAutomotive, Product } from "@/app/utils/products";
+import { useEffect, useState } from "react";
+import FilterBtnCategories from "../../utils/SortingUtils/FilterBtnCategories";
+import FilterBtnPrice from "../../utils/SortingUtils/FilterBtnPrice";
+import FilterButton from "../../utils/SortingUtils/FilterButton";
+import AutomotiveBrands from "./AutomotiveBrands";
+import AutomotiveProducts from "./AutomotiveProducts";
+
+enum SortOrder {
+  Relevance = "relevance",
+  Name = "name",
+  Low = "low",
+  High = "high",
+}
 
 export default function page() {
-  return <div>Automotive</div>;
+  const products = getProductsAutomotive();
+
+  const [sort, setSort] = useState(products);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
+  const [sortOrder, setSortOrder] = useState<SortOrder>(SortOrder.Relevance);
+  const [brands, setBrands] = useState<string[]>([]);
+  const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
+  const [value, setValue] = useState<[number, number]>([0, 14000]);
+  const getSortedProducts = (
+    filteredProducts: Product[],
+    sortOrder: SortOrder
+  ) => {
+    switch (sortOrder) {
+      case SortOrder.Relevance:
+        return filteredProducts.sort((a, b) => a.id - b.id);
+      case SortOrder.Name:
+        return filteredProducts.sort((a, b) => a.title.localeCompare(b.title));
+      case SortOrder.Low:
+        return filteredProducts.sort((a, b) => a.price - b.price);
+      case SortOrder.High:
+        return filteredProducts.sort((a, b) => b.price - a.price);
+      default:
+        return filteredProducts;
+    }
+  };
+
+  useEffect(() => {
+    setSort(products);
+  }, [products]);
+
+  useEffect(() => {
+    setSelectedCategories(categories);
+  }, [categories]);
+
+  useEffect(() => {
+    setSelectedBrands(brands);
+  }, [brands]);
+
+  useEffect(() => {
+    if (selectedCategories.length === 0 && selectedBrands.length === 0) {
+      setSort(products);
+    } else {
+      const filteredProducts = products.filter(
+        (product) =>
+          (selectedCategories.length === 0 ||
+            selectedCategories.includes(product.category)) &&
+          (selectedBrands.length === 0 ||
+            selectedBrands.includes(product.brand?.toString() || ""))
+      );
+      setSort(filteredProducts);
+    }
+  }, [selectedCategories, selectedBrands]);
+
+  const handleChange = (value: string) => {
+    if (value === "low") {
+      setSort((prev) => [...prev].sort((a, b) => a.price - b.price));
+      setSortOrder(SortOrder.Low);
+    } else if (value === "high") {
+      setSort((prev) => [...prev].sort((a, b) => b.price - a.price));
+      setSortOrder(SortOrder.High);
+    } else if (value === "name") {
+      setSort((prev) =>
+        [...prev].sort((a, b) => a.title.localeCompare(b.title))
+      );
+      setSortOrder(SortOrder.Name);
+    } else if (value === "relevance") {
+      setSort((prev) => [...prev].sort((a, b) => a.id - b.id));
+      setSortOrder(SortOrder.Relevance);
+    }
+  };
+
+  const handleCategoryClick = (category: string) => {
+    const isSelected = categories.includes(category);
+
+    if (isSelected) {
+      setCategories((prev) => prev.filter((c) => c !== category));
+    } else {
+      setCategories((prev) => [...prev, category]);
+    }
+
+    setSelectedCategories(categories);
+
+    const sortedProducts = getSortedProducts(products, sortOrder);
+    const filteredProducts = sortedProducts.filter((product) =>
+      categories.includes(product.category)
+    );
+    setSort(filteredProducts);
+  };
+
+  const handleBrandClick = (brand: string) => {
+    const isSelected = brands.includes(brand);
+
+    if (isSelected) {
+      setBrands((prev) => prev.filter((c) => c !== brand));
+      setSelectedBrands((prev) => prev.filter((c) => c !== brand));
+    } else {
+      setBrands((prev) => [...prev, brand]);
+      setSelectedBrands((prev) => [...prev, brand]);
+    }
+
+    const sortedProducts = getSortedProducts(products, sortOrder);
+    const filteredProducts = sortedProducts.filter((product) =>
+      selectedBrands.includes(product.brand?.toString() || "")
+    );
+    setSort(filteredProducts);
+  };
+
+  const handlePriceChange = (values: [number, number]) => {
+    const filteredProducts = products.filter(
+      (product) =>
+        (selectedCategories.length === 0 ||
+          selectedCategories.includes(product.category)) &&
+        (selectedBrands.length === 0 ||
+          selectedBrands.includes(product.brand?.toString() || "")) &&
+        product.price >= values[0] &&
+        product.price <= values[1]
+    );
+    setSort(filteredProducts);
+  };
+
+  return (
+    <div className="p-4 xl:px-[5.4rem] 2xl:px-[7.7rem] dark:bg-[#181D25]">
+      <h1 className="text-2xl font-semibold mt-10">Automotive</h1>
+
+      <SortProductsBtn handleChange={handleChange} />
+
+      <div className="flex lg:gap-4 md:justify-start w-full">
+        <div className="lg:block hidden">
+          <FilterBtnCategories
+            handleCategoryClick={handleCategoryClick}
+            selectedCategories={selectedCategories}
+            currentCategory="Automotive"
+          />
+          <FilterBtnPrice
+            values={value}
+            setValue={setValue}
+            onChange={handlePriceChange}
+          />
+          <AutomotiveBrands
+            handleBrandClick={handleBrandClick}
+            selectedBrands={selectedBrands}
+            currentCategory="Automotive"
+          />
+        </div>
+        <div className="md:flex-1">
+          <AutomotiveProducts products={sort} />
+        </div>
+        <div className="lg:hidden">
+          <FilterButton
+            handleCategoryClick={handleCategoryClick}
+            selectedCategories={selectedCategories}
+            values={value}
+            setValue={setValue}
+            onChange={handlePriceChange}
+            handleBrandClick={handleBrandClick}
+            selectedBrands={selectedBrands}
+            currentCategory="Automotive"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }

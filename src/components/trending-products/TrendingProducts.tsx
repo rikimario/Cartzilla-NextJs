6f2f49b125e9deaa2ae250389ca9488a18d08a14
rikimario <mario_martinov@abv.bg>
2024-11-25 18:12:36 +0200

    fix products for trending products component

--- src/components/trending-products/TrendingProducts.tsx
+++ src/components/trending-products/TrendingProducts.tsx
@@ -1,23 +1,12 @@
 "use client";
+import { getProductsElectronics, Product } from "@/app/utils/products";
 import { ChevronRight, Heart, ShoppingCart, Star } from "lucide-react";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import { Card, CardContent } from "../ui/card";
+import Link from "next/link";
 
 export default function TrendingProducts() {
-  const [shoes, setShoes] = useState([]);
-  const [watches, setWatches] = useState([]);
-
-  useEffect(() => {
-    fetch("https://dummyjson.com/products/category/mens-shoes")
-      .then((res) => res.json())
-      .then((data) => setShoes(data.products.slice(0, 4)));
-  }, []);
-
-  useEffect(() => {
-    fetch("https://dummyjson.com/products/category/mens-watches")
-      .then((res) => res.json())
-      .then((data) => setWatches(data.products.slice(0, 4)));
-  }, []);
+  const products = getProductsElectronics();
   return (
     <div className="p-4 dark:bg-[#181D25]">
       <div className="flex items-center justify-between border-b-[1px] border-gray-200">
@@ -29,114 +18,105 @@ export default function TrendingProducts() {
         </span>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-4 lg:gap-2 lg:pt-4 items-center justify-center justify-items-center text-center">
-        {shoes.map(
-          (item: {
-            title: string;
-            price: string;
-            thumbnail: string;
-            id: number;
-          }) => (
-            <div
-              className="relative flex flex-col text-center items-center gap-2 lg:min-w-[250px] pb-8 hover:shadow-lg transition-transform duration-600"
-              key={item.id}
-            >
-              <div className="absolute top-2 right-1">
-                <button className="bg-gray-200 dark:bg-gray-700 p-2 rounded-lg">
-                  <Heart
-                    className="h-3 w-3 lg:w-5 lg:h-5 text-gray-900 dark:text-white"
-                    strokeWidth={1}
-                  />
-                </button>
-              </div>
-              <Image
-                className="md:w-[160px] md:h-[160px] lg:w-[200px] lg:h-[200px]"
-                src={item.thumbnail}
-                alt={item.title}
-                width={120}
-                height={120}
-              />
-              <div className="flex flex-col gap-2  w-full text-start p-2">
-                <div className="flex">
-                  {Array.from({ length: 5 }).map((_, index) => (
-                    <Star
-                      key={index}
-                      className="text-[#FC9231] lg:w-5 lg:h-5"
-                      size={14}
-                      fill="#FC9231"
-                      strokeWidth={0}
+      <div className="px-2 lg:px-20 grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-4 lg:gap-2 lg:pt-4 items-center justify-center justify-items-center text-center">
+        {products.slice(28, 36).map((item: Product) => (
+          <div className="p-1 grid grid-cols-1 grid-rows-1 h-full w-full">
+            <Card className="dark:bg-[#181D25] h-full w-full">
+              <CardContent className="relative flex flex-col aspect-square items-center justify-center p-2">
+                <div className="absolute top-2 right-4">
+                  <button className="bg-gray-200 dark:bg-gray-700 p-1 md:p-2 rounded-lg">
+                    <Heart
+                      className="h-3 w-3 lg:w-5 lg:h-5 text-gray-900 dark:text-white"
+                      strokeWidth={1}
                     />
-                  ))}
-                </div>
-                <h1 className="text-xs lg:text-base text-gray-900 dark:text-white font-medium md:truncate md:overflow-ellipsis">
-                  {item.title}
-                </h1>
-                <div className="flex items-center justify-between pt-4">
-                  <p className="text-base lg:text-lg text-gray-900 dark:text-white font-semibold">
-                    ${item.price}
-                  </p>
-                  <button className="bg-gray-200 dark:bg-gray-700 p-3 rounded-xl">
-                    <ShoppingCart className="h-4 w-4" strokeWidth={1} />
                   </button>
                 </div>
-              </div>
-            </div>
-          )
-        )}
-
-        {watches.map(
-          (item: {
-            title: string;
-            price: string;
-            thumbnail: string;
-            id: number;
-          }) => (
-            <div
-              className="relative flex flex-col text-center items-center gap-2 lg:min-w-[250px] pb-8 hover:shadow-lg transition-transform duration-600"
-              key={item.id}
-            >
-              <div className="absolute top-1 right-1">
-                <button className="bg-gray-200 dark:bg-gray-700 p-2 rounded-lg">
-                  <Heart
-                    className="h-3 w-3 lg:w-5 lg:h-5 text-gray-900 dark:text-white"
-                    strokeWidth={1}
+                <Link href={`all-electronics/${item.id}`}>
+                  <Image
+                    className="lg:w-[200px] lg:h-[200px]"
+                    src={item.thumbnail}
+                    alt={item.title}
+                    width={160}
+                    height={160}
                   />
-                </button>
-              </div>
-              <Image
-                className="md:w-[160px] md:h-[160px] lg:w-[200px] lg:h-[200px]"
-                src={item.thumbnail}
-                alt={item.title}
-                width={120}
-                height={120}
-              />
-              <div className="text-start flex flex-col gap-2 w-full p-2">
-                <div className="flex">
-                  {Array.from({ length: 5 }).map((_, index) => (
-                    <Star
-                      key={index}
-                      className="text-[#FC9231] lg:w-5 lg:h-5"
-                      size={14}
-                      fill="#FC9231"
-                      strokeWidth={0}
-                    />
-                  ))}
-                </div>
-                <h1 className="text-xs lg:text-base text-gray-900 dark:text-white font-medium md:truncate md:overflow-ellipsis">
-                  {item.title}
-                </h1>
-                <div className="flex items-center justify-between pt-4 gap-2">
-                  <p className="text-base lg:text-lg text-gray-900 dark:text-white font-semibold">
-                    ${item.price}
+                </Link>
+                <div className="flex flex-col gap-2 w-full text-start p-2">
+                  <div className="flex items-center text-center">
+                    {Array.from({ length: 5 }).map((_, index) =>
+                      item.rating >= index + 1 ? (
+                        <Star
+                          key={index}
+                          className="text-[#FC9231] lg:w-5 lg:h-5"
+                          size={16}
+                          fill="#FC9231"
+                          strokeWidth={0}
+                        />
+                      ) : item.rating > index && item.rating < index + 1 ? (
+                        <div style={{ position: "relative" }}>
+                          <Star
+                            key={index}
+                            className="text-[#FC9231] lg:w-5 lg:h-5"
+                            size={16}
+                            fill="#FC9231"
+                            strokeWidth={0}
+                            style={{
+                              clipPath: "polygon(0 0, 50% 0, 50% 100%, 0 100%)",
+                            }}
+                          />
+                          <Star
+                            key={index + 5}
+                            className="text-gray-400 lg:w-5 lg:h-5"
+                            size={16}
+                            fill="none"
+                            stroke="#999897"
+                            strokeWidth={1}
+                            style={{
+                              position: "absolute",
+                              top: 0,
+                              left: 0,
+                            }}
+                          />
+                        </div>
+                      ) : (
+                        <Star
+                          key={index}
+                          className="text-gray-400 lg:w-5 lg:h-5"
+                          size={16}
+                          fill="none"
+                          stroke="#999897"
+                          strokeWidth={1}
+                        />
+                      )
+                    )}
+                    <p className="pl-2 text-xs md:text-sm text-gray-400 flex items-center">
+                      ({item.rating})
+                    </p>
+                  </div>
+                  <h1 className="text-xs lg:text-lg text-gray-900 dark:text-white font-medium truncate overflow-ellipsis">
+                    {item.title}
+                  </h1>
+                  <div className="flex items-center justify-between pt-2">
+                    <p className="text-xs lg:text-2xl text-gray-900 dark:text-white font-semibold">
+                      ${item.price}
+                    </p>
+                    <button className="bg-gray-200 dark:bg-gray-700 p-2 md:p-3 rounded-xl">
+                      <ShoppingCart
+                        className="h-3 w-3 md:h-4 md:w-4"
+                        strokeWidth={1}
+                      />
+                    </button>
+                  </div>
+                  <p className="flex text-xs md:text-base gap-2 text-gray-400">
+                    Available:{" "}
+                    <span className="text-gray-900 dark:text-white text-xs md:text-base">
+                      {item.stock}
+                    </span>
                   </p>
-                  <button className="bg-gray-200 dark:bg-gray-700 p-3 rounded-xl">
-                    <ShoppingCart className="h-4 w-4" strokeWidth={1} />
-                  </button>
                 </div>
-              </div>
-            </div>
-          )
-        )}
+              </CardContent>
+            </Card>
+          </div>
+        ))}
       </div>
     </div>
   );

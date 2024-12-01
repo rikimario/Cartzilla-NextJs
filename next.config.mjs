f4968e85afeb58be2a227b1603061d046a2463f6
rikimario <mario_martinov@abv.bg>
2024-12-01 23:15:09 +0200

    add env for supabase

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["cdn.dummyjson.com"],
   },
+  env: {
+    NEXT_APP_SUPABASE_URL: process.env.NEXT_APP_SUPABASE_URL,
+    NEXT_APP_SUPABASE_KEY: process.env.NEXT_APP_SUPABASE_KEY,
+  },
 };
 
 export default nextConfig;

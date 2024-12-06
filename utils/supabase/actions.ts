5ce5d0419af5321064406e5d7dc9028f03af469c
rikimario <mario_martinov@abv.bg>
2024-12-06 21:38:22 +0200

    add sign with google actions function

--- utils/supabase/actions.ts
+++ utils/supabase/actions.ts
@@ -50,3 +50,24 @@ export async function logout() {
   revalidatePath("/", "layout");
   redirect("/");
 }
+
+export async function signInWithGoogle() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: "http://localhost:3000/auth/callback",
+      queryParams: {
+        access_type: "offline",
+        prompt: "consent",
+      },
+    },
+  });
+
+  if (error) {
+    console.log(error);
+    redirect("/error");
+  }
+
+  redirect(data.url);
+}

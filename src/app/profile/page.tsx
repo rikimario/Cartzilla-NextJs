264453ce257ce9b257bcde1da7d09defe3cc0971
rikimario <mario_martinov@abv.bg>
2024-12-06 21:40:39 +0200

    get current user for profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { createClient } from "../../../utils/supabase/server";
 
-export default function Profile() {
-  return <div>Profile</div>;
+export default async function Profile() {
+  const supabase = await createClient();
+
+  const { data: user } = await supabase.auth.getUser();
+  return <div>This is profile ID: {user.user?.email}</div>;
 }

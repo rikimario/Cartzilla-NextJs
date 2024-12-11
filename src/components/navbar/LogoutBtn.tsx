042a29caf25e2246e045aff1cb2d5b05e96aac8d
rikimario <mario_martinov@abv.bg>
2024-12-11 23:40:14 +0200

    add css to logout btn

--- src/components/navbar/LogoutBtn.tsx
+++ src/components/navbar/LogoutBtn.tsx
@@ -4,10 +4,11 @@ import { logout } from "../../../utils/supabase/actions";
 export default function LogoutBtn() {
   return (
     <>
-      <button onClick={logout}>
-        <LogOut className="h-5 w-5 hidden lg:block" strokeWidth={1}>
-          Logout
-        </LogOut>
+      <button className="flex gap-2 items-center ml-4" onClick={logout}>
+        <LogOut className="h-5 w-5 hidden lg:block" strokeWidth={1} />
+        Logout
+        {/* Logout
+        </LogOut> */}
       </button>
     </>
   );

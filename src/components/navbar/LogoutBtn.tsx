069c73c8db43f5da431ba83391df71c9191195a7
rikimario <mario_martinov@abv.bg>
2024-12-13 11:03:21 +0200

    fix margin

--- src/components/navbar/LogoutBtn.tsx
+++ src/components/navbar/LogoutBtn.tsx
@@ -4,7 +4,7 @@ import { logout } from "../../../utils/supabase/actions";
 export default function LogoutBtn() {
   return (
     <>
-      <button className="flex gap-2 items-center ml-4" onClick={logout}>
+      <button className="flex gap-2 items-center ml-2" onClick={logout}>
         <LogOut className="h-5 w-5 hidden lg:block" strokeWidth={1} />
         Logout
         {/* Logout

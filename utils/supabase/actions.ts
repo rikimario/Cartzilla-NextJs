06ccc74d5642fa03fcc80a6c619bbdb9b6946fc9
rikimario <mario_martinov@abv.bg>
2024-12-13 11:01:22 +0200

    add username sign up option

--- utils/supabase/actions.ts
+++ utils/supabase/actions.ts
@@ -4,11 +4,10 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { createClient } from "./server";
 
+//* Login
 export async function login(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
@@ -24,20 +23,24 @@ export async function login(formData: FormData) {
   redirect("/");
 }
 
+//* Register
 export async function signup(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
+    options: {
+      data: {
+        full_name: formData.get("full_name") as string,
+      },
+    },
   };
 
-  const { error } = await supabase.auth.signUp(data);
+  const { error: signUpError } = await supabase.auth.signUp(data);
 
-  if (error) {
-    redirect("/error");
+  if (signUpError) {
+    return { error: "An error occurred while signing up" };
   }
 
   revalidatePath("/", "layout");
@@ -51,6 +54,7 @@ export async function logout() {
   redirect("/");
 }
 
+//* Sign in with Google
 export async function signInWithGoogle() {
   const supabase = await createClient();
   const { data, error } = await supabase.auth.signInWithOAuth({

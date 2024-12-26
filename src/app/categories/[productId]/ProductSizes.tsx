44abe8947e1b1fa776b080d7b71d0807abbcc728
rikimario <mario_martinov@abv.bg>
2024-12-26 22:56:22 +0200

    use search params to keep the picked size in the url

--- src/app/categories/[productId]/ProductSizes.tsx
+++ src/app/categories/[productId]/ProductSizes.tsx
@@ -1,30 +1,28 @@
 import { Product } from "@/app/utils/products";
-import React, { useState } from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 interface SizesOptionsProps {
   product: Product;
 }
 export default function ProductSizes({ product }: SizesOptionsProps) {
-  const [size, setSize] = useState<string>("");
-
-  const handleClick = (size: string) => {
-    setSize(size);
-  };
+  const searchParams = useSearchParams();
+  const selectedSize = searchParams.get("size");
 
   return (
     <div className="flex flex-wrap gap-4 md:mt-4 mt-8 lg:w-2/3">
       {product.size?.map((s, index) => (
-        <button
+        <Link
+          href={`?size=${s}`}
           key={index}
           className={`flex items-center justify-center p-2 border border-gray-300 hover:border-gray-700 min-w-10 md:min-w-32 lg:min-w-10 ${
-            s === size
+            selectedSize === s
               ? "border-gray-800 bg-gray-800 text-white"
               : "border-gray-300"
           }`}
-          onClick={() => handleClick(s)}
         >
-          <p className="">{s}</p>
-        </button>
+          <p>{s}</p>
+        </Link>
       ))}
     </div>
   );

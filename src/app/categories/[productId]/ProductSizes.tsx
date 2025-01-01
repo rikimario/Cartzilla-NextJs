3aeea4c2518664241010f44d4c9a4825262bcf83
rikimario <mario_martinov@abv.bg>
2025-01-01 23:27:46 +0200

    fix an issue where the back btn goes to previous size rather then the previous page

--- src/app/categories/[productId]/ProductSizes.tsx
+++ src/app/categories/[productId]/ProductSizes.tsx
@@ -1,19 +1,25 @@
 import { Product } from "@/app/utils/products";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface SizesOptionsProps {
   product: Product;
 }
 export default function ProductSizes({ product }: SizesOptionsProps) {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const selectedSize = searchParams.get("size");
+
+  const handleSizeClick = (size: string) => {
+    const url = new URLSearchParams(window.location.search);
+    url.set("size", size);
+    router.replace(`${window.location.pathname}?${url.toString()}`, undefined);
+  };
 
   return (
     <div className="flex flex-wrap gap-4 md:mt-4 mt-8 lg:w-2/3">
       {product.size?.map((s, index) => (
-        <Link
-          href={`?size=${s}`}
+        <button
+          onClick={() => handleSizeClick(s)}
           key={index}
           className={`flex items-center justify-center p-2 border border-gray-300 hover:border-gray-700 min-w-10 md:min-w-32 lg:min-w-10 ${
             selectedSize === s
@@ -22,7 +28,7 @@ export default function ProductSizes({ product }: SizesOptionsProps) {
           }`}
         >
           <p>{s}</p>
-        </Link>
+        </button>
       ))}
     </div>
   );

53f22a195f502207169b98a23d8c54a3e5f41fed
rikimario <mario_martinov@abv.bg>
2025-01-09 23:19:50 +0200

    use handle brand function and keep checked brand in url

--- src/app/categories/all-men/MenBrands.tsx
+++ src/app/categories/all-men/MenBrands.tsx
@@ -1,66 +1,72 @@
 "use client";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
-import {
-  getProductsAutomotive,
-  getProductsCosmetics,
-  getProductsElectronics,
-  getProductsHomeAndKitchen,
-  getProductsMen,
-  getProductsSportsAccessories,
-  getProductsSunglasses,
-  getProductsWomen,
-} from "@/app/utils/products";
-
-interface FilterBtnBrandsProps {
-  handleBrandClick: (brand: string) => void;
-  selectedBrands: string[];
-  currentCategory: string;
-}
+import { Product } from "@/lib/types";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useMemo } from "react";
 
 export default function MenBrands({
-  handleBrandClick,
+  products = [],
   selectedBrands,
-  currentCategory,
-}: FilterBtnBrandsProps) {
-  // const products = getProductsMen();
-  let products;
-  if (currentCategory === "Men") {
-    products = getProductsMen();
-  } else if (currentCategory === "Women") {
-    products = getProductsWomen();
-  } else if (currentCategory === "Electronics") {
-    products = getProductsElectronics();
-  } else if (currentCategory === "Home & Kitchen") {
-    products = getProductsHomeAndKitchen();
-  } else if (currentCategory === "Cosmetics") {
-    products = getProductsCosmetics();
-  } else if (currentCategory === "Automotive") {
-    products = getProductsAutomotive();
-  } else if (currentCategory === "Sports Accessories") {
-    products = getProductsSportsAccessories();
-  } else if (currentCategory === "Sunglasses") {
-    products = getProductsSunglasses();
-  } else {
-    // Handle other categories or default to one of the above
-    products = getProductsMen(); // or getProductsWomen()
-  }
+  onBrandChange,
+  onFilteredProducts,
+}: {
+  products?: Product[];
+  selectedBrands: string[];
+  onBrandChange: (selectedBrands: string[]) => void;
+  onFilteredProducts: (filteredProducts: Product[]) => void;
+}) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const selectedBrand = useMemo(
+    () => searchParams.getAll("brand"),
+    [searchParams]
+  );
 
+  const filteredProducts = useMemo(() => {
+    return selectedBrand.length === 0
+      ? products
+      : products?.filter((product) =>
+          selectedBrand.includes(product.brand || "")
+        );
+  }, [products, selectedBrand]);
+
+  useEffect(() => {
+    onFilteredProducts(filteredProducts);
+  }, [filteredProducts, onFilteredProducts]);
+
+  const handleBrandClick = (brand: string) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const currentBrand = urlParams.getAll("brand");
+
+    if (currentBrand.includes(brand)) {
+      const updatedBrand = currentBrand.filter((b) => b !== brand);
+      urlParams.delete("brand");
+      updatedBrand.forEach((b) => urlParams.append("brand", b));
+    } else {
+      urlParams.append("brand", brand);
+    }
+
+    router.replace(`${window.location.pathname}?${urlParams.toString()}`);
+
+    const newSelectedBrand = urlParams.getAll("brand");
+    onBrandChange(newSelectedBrand);
+  };
   return (
     <div className="p-6 border border-gray-200 rounded-xl text-gray-700 dark:text-white mt-6">
       <h2 className="text-start text-xl text-gray-700 dark:text-white font-semibold">
         Brand
       </h2>
       <div className="relative flex flex-col items-start gap-2 mt-4">
-        {Array.from(new Set(products.map((item) => item.brand))).map(
+        {Array.from(new Set(products?.map((item) => item.brand))).map(
           (brand) => (
             <span
               key={brand}
-              onClick={() => handleBrandClick(brand)}
+              onClick={() => handleBrandClick(brand ?? "")}
               className="flex items-center space-x-2"
             >
               <Checkbox
-                checked={selectedBrands.includes(brand)}
+                checked={selectedBrands.includes(brand ?? "")}
                 id={brand}
                 key={brand}
               />

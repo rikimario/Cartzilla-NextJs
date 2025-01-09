8d0a7b79919489d8de318a8314f6f37964b5ad36
rikimario <mario_martinov@abv.bg>
2025-01-09 23:22:43 +0200

    use sorte product function in sort product component and keep selected sort in url

--- src/app/utils/SortingUtils/SortProductsBtn.tsx
+++ src/app/utils/SortingUtils/SortProductsBtn.tsx
@@ -6,10 +6,9 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
-interface SortProductsBtnProps {
-  handleChange: (value: string) => void;
-}
+import { SortOrder } from "@/lib/types";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useMemo } from "react";
 
 const SORT_OPTIONS = [
   { value: "relevance", name: "Relevance" },
@@ -19,14 +18,34 @@ const SORT_OPTIONS = [
 ] as const;
 
 export default function SortProductsBtn({
-  handleChange,
-}: SortProductsBtnProps) {
+  handleSortedProductsChange,
+}: {
+  handleSortedProductsChange: (sortOrder: SortOrder) => void;
+}) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const selectedSort = useMemo(
+    () => searchParams.getAll("sort") || "relevance",
+    [searchParams]
+  );
+  const handleSortChange = (value: string) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("sort", value);
+
+    // Replace the URL without reloading the page
+    router.replace(`${window.location.pathname}?${urlParams.toString()}`);
+
+    handleSortedProductsChange(value as SortOrder);
+  };
   return (
     <div className="flex items-center lg:justify-end mt-4 text-nowrap">
       <label className="text-sm font-semibold text-gray-900 dark:text-white">
         Sort by:
       </label>
-      <Select onValueChange={handleChange}>
+      <Select
+        value={selectedSort[0] || "relevance"}
+        onValueChange={handleSortChange}
+      >
         <SelectTrigger className="w-44 dark:bg-inherit text-gray-600 dark:text-white border-none">
           <SelectValue className="text-gray-300" placeholder="Relevance" />
         </SelectTrigger>

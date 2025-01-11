0602e2782207f3d7b4e90903651153b5ae172729
rikimario <mario_martinov@abv.bg>
2025-01-11 21:26:07 +0200

    move to separate utils

---
+++ src/app/utils/Brands.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { Product } from "@/lib/types";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useMemo } from "react";
+
+export default function Brands({
+  products = [],
+  selectedBrands,
+  onBrandChange,
+  onFilteredProducts,
+}: {
+  products?: Product[];
+  selectedBrands: string[];
+  onBrandChange: (selectedBrands: string[]) => void;
+  onFilteredProducts: (filteredProducts: Product[]) => void;
+}) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const selectedBrand = useMemo(
+    () => searchParams.getAll("brand"),
+    [searchParams]
+  );
+
+  const filteredProducts = useMemo(() => {
+    return selectedBrand.length === 0
+      ? products
+      : products?.filter((product) =>
+          selectedBrand.includes(product.brand || "")
+        );
+  }, [products, selectedBrand]);
+
+  useEffect(() => {
+    onFilteredProducts(filteredProducts);
+  }, [filteredProducts, onFilteredProducts]);
+
+  const handleBrandClick = (brand: string) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const currentBrand = urlParams.getAll("brand");
+
+    if (currentBrand.includes(brand)) {
+      const updatedBrand = currentBrand.filter((b) => b !== brand);
+      urlParams.delete("brand");
+      updatedBrand.forEach((b) => urlParams.append("brand", b));
+    } else {
+      urlParams.append("brand", brand);
+    }
+
+    router.replace(`${window.location.pathname}?${urlParams.toString()}`);
+
+    const newSelectedBrand = urlParams.getAll("brand");
+    onBrandChange(newSelectedBrand);
+  };
+  return (
+    <div className="p-6 border border-gray-200 rounded-xl text-gray-700 dark:text-white mt-6">
+      <h2 className="text-start text-xl text-gray-700 dark:text-white font-semibold">
+        Brand
+      </h2>
+      <div className="relative flex flex-col items-start gap-2 mt-4">
+        {Array.from(new Set(products?.map((item) => item.brand))).map(
+          (brand) => (
+            <span
+              key={brand}
+              onClick={() => handleBrandClick(brand ?? "")}
+              className="flex items-center space-x-2"
+            >
+              <Checkbox
+                checked={selectedBrands.includes(brand ?? "")}
+                id={brand}
+                key={brand}
+              />
+              <Label htmlFor={brand}>{brand}</Label>
+            </span>
+          )
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/utils/ProductsMain.tsx
@@ -0,0 +1,32 @@
+"use client";
+import ProductCard from "@/app/utils/ProductCard";
+import ProductSkeleton from "@/app/utils/ProductSkeleton";
+import { Product } from "@/lib/types";
+import { useEffect, useState } from "react";
+
+export default function ProductsMain({ products }: { products: Product[] }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (products) {
+      setIsLoading(false);
+    }
+  }, [products]);
+  return (
+    <div className="py-10 dark:bg-[#181D25]">
+      <p className="text-lg ml-1 text-gray-600 dark:text-gray-300 font-light">
+        Found <span className="font-bold text-gray-700">{products.length}</span>{" "}
+        items
+      </p>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-4 lg:gap-2 lg:pt-4 items-center justify-center justify-items-center text-center grid-auto-columns">
+        {isLoading
+          ? Array.from({ length: 8 }).map((_, index) => (
+              <ProductSkeleton key={index} />
+            ))
+          : products.map((product, index) => (
+              <ProductCard key={product.product_id} product={product} />
+            ))}
+      </div>
+    </div>
+  );
+}

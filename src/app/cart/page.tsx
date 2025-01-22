9eb24f38ea2214cee105b285e667acf1c3d885be
rikimario <mario_martinov@abv.bg>
2025-01-22 23:36:24 +0200

    select products form supabase cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,56 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { createClient } from "../../../utils/supabase/client";
+import { Product } from "@/lib/types";
 
 export default function Cart() {
-  return <div>Cart</div>;
+  const [product, setProduct] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const supabase = await createClient();
+
+        const { data: product, error } = await supabase
+          .from("cart")
+          .select("*");
+
+        if (error) throw error;
+
+        const groupedProducts = product.reduce((acc, product) => {
+          const existingProduct = acc.find(
+            (p: Product) => p.product_id === product.product_id
+          );
+          if (existingProduct) {
+            existingProduct.quantity += product.quantity;
+          } else {
+            acc.push(product);
+          }
+          return acc;
+        }, [] as Product[]);
+
+        setProduct(groupedProducts.reverse());
+      } catch (error) {
+        console.error("Error fetching cart:", error);
+      }
+    };
+    fetchProduct();
+  }, []);
+
+  return (
+    <div>
+      <h1>Cart</h1>
+      {product.map((product) => (
+        <div key={product.product_id}>
+          <div className="flex gap-4">
+            <img src={product.thumbnail} alt={product.title} />
+            <h2>{product.title}</h2>
+            <p>Price: ${product.price}</p>
+            <p>Quantity: {product.quantity}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }

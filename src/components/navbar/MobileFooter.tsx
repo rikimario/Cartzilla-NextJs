9318d3d2dc6c7e02721fc16ba4f20ebbd6eeacf1
rikimario <mario_martinov@abv.bg>
2025-01-27 21:38:02 +0200

    add correct links for profile and favorite btns

--- src/components/navbar/MobileFooter.tsx
+++ src/components/navbar/MobileFooter.tsx
@@ -1,4 +1,4 @@
-import { Heart, User, UserCheck } from "lucide-react";
+import { Heart, User } from "lucide-react";
 import React from "react";
 import { createClient } from "../../../utils/supabase/server";
 import Link from "next/link";
@@ -10,7 +10,7 @@ export default async function MobileFooter() {
   return (
     <div className="absolute flex bottom-0 left-0 right-0 bg-inherit py-6 border-t-2 border-gray-200 items-center justify-center gap-12">
       {data.user ? (
-        <Link href={"/profile"}>
+        <Link href={`/profile?tab=Orders`}>
           <button className="flex items-center justify-center w-full p-2 gap-2">
             Account
           </button>
@@ -25,12 +25,15 @@ export default async function MobileFooter() {
           </button>
         </Link>
       )}
-      <div className="flex items-center justify-center p-2 gap-2">
+      <Link
+        href={`/profile?tab=Wishlist`}
+        className="flex items-center justify-center p-2 gap-2"
+      >
         <span>
           <Heart className="h-6 w-6" strokeWidth={1} />
         </span>
         Wishlist
-      </div>
+      </Link>
     </div>
   );
 }

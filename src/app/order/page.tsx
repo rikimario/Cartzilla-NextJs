e1918cdb72b013a4ca880b4e23538c251f0d1937
rikimario <mario_martinov@abv.bg>
2025-01-30 21:07:12 +0200

    create order page

---
+++ src/app/order/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import OrderLeftBlock from "./_components/OrderLeftBlock";
+
+export default function page() {
+  return (
+    <div className="py-10 p-4 md:px-12 lg:px-4 xl:px-[7.5rem]">
+      <OrderLeftBlock />
+    </div>
+  );
+}

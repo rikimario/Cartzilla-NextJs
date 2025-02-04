9af5034ce1d8cc6d1c14e9d923888744e4bb88bb
rikimario <mario_martinov@abv.bg>
2025-02-04 22:44:21 +0200

    rename

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import OrderLeftBlock from "./_components/OrderLeftBlock";
+import Order from "./_components/Order";
 
 export default function page() {
   return (
     <div className="py-10 p-4 md:px-12 lg:px-4 xl:px-[7.5rem]">
-      <OrderLeftBlock />
+      <Order />
     </div>
   );
 }

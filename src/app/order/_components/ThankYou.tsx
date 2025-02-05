674286c7c8b97d184789f99846650952a4eb4db4
rikimario <mario_martinov@abv.bg>
2025-02-05 20:35:36 +0200

    small screen fixes

--- src/app/order/_components/ThankYou.tsx
+++ src/app/order/_components/ThankYou.tsx
@@ -18,18 +18,18 @@ export default function ThankYou({
   comment: string;
 }) {
   return (
-    <div className="flex items-center justify-between gap-16">
-      <div className="w-1/2">
+    <div className="lg:flex items-center justify-between gap-10 lg:gap-16 space-y-8 lg:space-y-0 px-2 lg:px-0">
+      <div className="lg:w-1/2 px-4 md:px-0">
         <div className="flex items-center gap-4 my-8">
           <span className="flex items-center justify-center p-4 text-lg bg-green-500 font-medium text-white rounded-full">
             <Check className="w-8 h-8" />
           </span>
-          <div className="flex items-center justify-between w-full">
+          <div className="md:flex items-center justify-between w-full space-y-4">
             <div>
               <p className="text-gray-500">
                 Order #{Math.floor(Math.random() * 1000000)}
               </p>
-              <h2 className="text-3xl font-medium">
+              <h2 className="lg:text-3xl text-xl md:font-medium font-semibold">
                 Thank you for your order!
               </h2>
             </div>
@@ -38,6 +38,7 @@ export default function ThankYou({
             </button>
           </div>
         </div>
+
         <div className="space-y-4 border-t border-gray-200 pt-4">
           <div className="space-y-2">
             <h2 className="text-lg font-semibold">Delivery</h2>
@@ -64,7 +65,7 @@ export default function ThankYou({
         </div>
       </div>
 
-      <div className="w-1/2 bg-emerald-100 text-center space-y-3 px-6 py-24 rounded-lg">
+      <div className="lg:w-1/2 bg-emerald-100 text-center space-y-3 px-6 py-24 rounded-lg">
         <h2 className="text-2xl font-semibold">
           🎉 Congratulations! 30% off your new purchase!
         </h2>
@@ -72,14 +73,14 @@ export default function ThankYou({
           Use your cupon now or look for it in your personal account.
         </p>
 
-        <div className="flex items-center justify-between px-16 py-4 gap-4">
+        <div className="flex flex-col lg:flex-row items-center justify-between lg:px-16 py-4 gap-4">
           <input
             type="text"
             className="w-full p-2 text-gray-500 bg-white border border-gray-300 rounded-xl"
             value="30%SALLOFF"
             readOnly={true}
           />
-          <Button className="dark:text-gray-200 dark:hover:text-white font-medium">
+          <Button className="dark:text-gray-200 dark:hover:text-white font-medium w-full">
             Copy cupon
           </Button>
         </div>

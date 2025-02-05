69f6674ea4312a83d5756cb36c27c819c0199039
rikimario <mario_martinov@abv.bg>
2025-02-05 20:33:49 +0200

    add p-4

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -3,7 +3,7 @@ import Order from "./_components/Order";
 
 export default function page() {
   return (
-    <div className="py-10 p-4 md:px-12 lg:px-4 xl:px-[7.5rem]">
+    <div className="py-10 md:px-12 lg:px-4 xl:px-[7.5rem]">
       <Order />
     </div>
   );

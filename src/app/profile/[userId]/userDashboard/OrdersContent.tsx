30d4ea21605718bc29cbfcc747970d87d21385ea
rikimario <mario_martinov@abv.bg>
2025-02-18 20:01:56 +0200

    fix view for small screen

--- src/app/profile/[userId]/userDashboard/OrdersContent.tsx
+++ src/app/profile/[userId]/userDashboard/OrdersContent.tsx
@@ -34,14 +34,14 @@ export default function OrdersContent() {
   }, []);
 
   return (
-    <div className="p-6">
+    <div className="p-2 md:p-6">
       <h1 className="text-4xl font-bold text-gray-700 mb-8 pb-2 dark:text-white">
         Orders
       </h1>
       <div className="flex text-gray-500 pb-4 border-b border-gray-200 dark:border-gray-600">
-        <span className="w-52">Order #</span>
-        <span className="w-52">Order date</span>
-        <span className="w-52">Total</span>
+        <span className="xl:w-52 w-44">Order #</span>
+        <span className="hidden md:block xl:w-52 w-44">Order date</span>
+        <span className="hidden md:block xl:w-52 w-44">Total</span>
       </div>
       {orders.length === 0 ? (
         <p>No orders found.</p>
@@ -51,16 +51,16 @@ export default function OrdersContent() {
             key={index}
             className="flex items-center justify-between py-4 border-b border-gray-200 dark:border-gray-600"
           >
-            <div className="flex">
-              <h3 className="w-52">{order.order_id}</h3>
-              <p className="w-52">
+            <div className="w-1/3 md:w-full md:flex space-y-4 md:space-y-0">
+              <h3 className="xl:w-52 w-44">{order.order_id}</h3>
+              <p className="text-gray-500 xl:w-52 w-44">
                 {new Date(order.created_at).toLocaleDateString("en-US", {
                   month: "short",
                   day: "numeric",
                   year: "numeric",
                 })}
               </p>
-              <p>${order.order_total.toFixed(2)}</p>
+              <p>${(order.order_total + 16.5).toFixed(2)}</p>
             </div>
 
             <OrderProductsSheet order={order} />

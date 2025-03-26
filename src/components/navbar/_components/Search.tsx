92a3d133f0f23851003c64936e01719b83f34979
rikimario <mario_martinov@abv.bg>
2025-03-26 23:57:36 +0200

    add scroll functionality

--- src/components/navbar/_components/Search.tsx
+++ src/components/navbar/_components/Search.tsx
@@ -5,6 +5,7 @@ import { searchProducts } from "../../../../utils/supabase/actions";
 import Link from "next/link";
 import Image from "next/image";
 import { Product } from "@/lib/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Search() {
   const [searchQuery, setSearchQuery] = useState<string>("");
@@ -23,33 +24,40 @@ export default function Search() {
   }, [searchQuery]);
   return (
     <div className="hidden lg:flex gap-8">
-      <input
-        className="h-10 max-w-96 text-lg overflow-auto bg-[#333e4e] outline-none border-none text-[#E0E5EB] rounded-full pl-4 pr-12"
-        type="text"
-        placeholder="Search"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      {searchResults.length > 0 && (
-        <div className="absolute top-0 left-0 bg-white shadow-md p-4 w-96">
-          <h2 className="text-lg font-bold mb-2">Search Results</h2>
-          <ul>
-            {searchResults.map((result) => (
-              <li key={result.product_id}>
-                <Link href={`/categories/${result.product_id}`}>
-                  <span>{result.title}</span>
-                  <Image
-                    width={50}
-                    height={50}
-                    src={result.thumbnail}
-                    alt={result.title}
-                  />
-                </Link>
-              </li>
-            ))}
-          </ul>
+      <div className="felx flex-col">
+        <input
+          className="h-10 max-w-96 text-lg overflow-auto bg-[#333e4e] outline-none border-none text-[#E0E5EB] rounded-full pl-4 pr-12"
+          type="text"
+          placeholder="Search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+
+        <div className="absolute top-16 z-50">
+          {searchResults.length > 0 && (
+            <ScrollArea className="bg-white rounded-lg">
+              <div className="p-4 w-72 h-96">
+                <h2 className="text-lg font-bold mb-2">Search Results</h2>
+                <ul>
+                  {searchResults.map((result) => (
+                    <li key={result.product_id}>
+                      <Link href={`/categories/${result.product_id}`}>
+                        <span>{result.title}</span>
+                        <Image
+                          width={100}
+                          height={100}
+                          src={result.thumbnail}
+                          alt={result.title}
+                        />
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </ScrollArea>
+          )}
         </div>
-      )}
+      </div>
       <div className="flex gap-2 items-center">
         <div className="bg-[#333D4C] p-3 h-10 w-10 rounded-full flex items-center justify-center">
           <span className="text-[#F55266]">%</span>

0c6fed7609b4a7ee354905825137376bcc7333fc
rikimario <mario_martinov@abv.bg>
2025-04-09 23:50:27 +0300

    sort reviews by date

--- src/app/categories/[productId]/productReviews/ProductComments.tsx
+++ src/app/categories/[productId]/productReviews/ProductComments.tsx
@@ -4,7 +4,6 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import { Product } from "@/lib/types";
 import {
   ChevronRight,
   CornerDownRight,
@@ -21,7 +20,7 @@ interface ReviewState {
   };
 }
 
-export default function ProductComments({ product }: { product: Product }) {
+export default function ProductComments({ reviews }: { reviews: any[] }) {
   const [reviewState, setReviewState] = useState<ReviewState>({});
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -74,104 +73,106 @@ export default function ProductComments({ product }: { product: Product }) {
     return date.toLocaleDateString("en-US", options);
   };
 
-  const sortedReviews = [...(product.reviews || [])].sort((a, b) => {
-    const dateA = new Date(a.date || 0).getTime();
-    const dateB = new Date(b.date || 0).getTime();
-    return dateB - dateA;
-  });
   return (
     <div className="mt-10">
-      {sortedReviews.slice(0, 2).map((review, index) => (
-        <div key={index} className="p-2 border-b-2 border-gray-200">
-          <div className="flex justify-between py-4">
-            <div>
-              <p className="text-gray-900 dark:text-white text-lg font-semibold">
-                {review.reviewerName}
-              </p>
-              <span className="flex items-center text-center mt-4">
-                {Array.from({ length: 5 }).map((_, index) =>
-                  review.rating >= index + 1 ? (
-                    <Star
-                      key={index}
-                      className="text-[#FC9231] lg:w-5 lg:h-5"
-                      size={16}
-                      fill="#FC9231"
-                      strokeWidth={0}
-                    />
-                  ) : review.rating > index && review.rating < index + 1 ? (
-                    <div key={index} style={{ position: "relative" }}>
+      {reviews
+        .sort((a, b) => {
+          const dateA = new Date(a.date || 0).getTime();
+          const dateB = new Date(b.date || 0).getTime();
+          return dateB - dateA;
+        })
+        .slice(0, 2)
+        .map((review, index) => (
+          <div key={index} className="p-2 border-b-2 border-gray-200">
+            <div className="flex justify-between py-4">
+              <div>
+                <p className="text-gray-900 dark:text-white text-lg font-semibold">
+                  {review.reviewerName}
+                </p>
+                <span className="flex items-center text-center mt-4">
+                  {Array.from({ length: 5 }).map((_, index) =>
+                    review.rating >= index + 1 ? (
                       <Star
                         key={index}
                         className="text-[#FC9231] lg:w-5 lg:h-5"
                         size={16}
                         fill="#FC9231"
                         strokeWidth={0}
-                        style={{
-                          clipPath: "polygon(0 0, 50% 0, 50% 100%, 0 100%)",
-                        }}
                       />
+                    ) : review.rating > index && review.rating < index + 1 ? (
+                      <div key={index} style={{ position: "relative" }}>
+                        <Star
+                          key={index}
+                          className="text-[#FC9231] lg:w-5 lg:h-5"
+                          size={16}
+                          fill="#FC9231"
+                          strokeWidth={0}
+                          style={{
+                            clipPath: "polygon(0 0, 50% 0, 50% 100%, 0 100%)",
+                          }}
+                        />
+                        <Star
+                          key={index + 5}
+                          className="text-gray-400 lg:w-5 lg:h-5"
+                          size={16}
+                          fill="none"
+                          stroke="#999897"
+                          strokeWidth={1}
+                          style={{
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                          }}
+                        />
+                      </div>
+                    ) : (
                       <Star
-                        key={index + 5}
+                        key={index}
                         className="text-gray-400 lg:w-5 lg:h-5"
                         size={16}
                         fill="none"
                         stroke="#999897"
                         strokeWidth={1}
-                        style={{
-                          position: "absolute",
-                          top: 0,
-                          left: 0,
-                        }}
                       />
-                    </div>
-                  ) : (
-                    <Star
-                      key={index}
-                      className="text-gray-400 lg:w-5 lg:h-5"
-                      size={16}
-                      fill="none"
-                      stroke="#999897"
-                      strokeWidth={1}
-                    />
-                  )
-                )}
-              </span>
+                    )
+                  )}
+                </span>
+              </div>
+              <p className="text-gray-500 text-sm">{formatDate(review.date)}</p>
             </div>
-            <p className="text-gray-500 text-sm">{formatDate(review.date)}</p>
-          </div>
-          <p className="text-gray-600 dark:text-gray-400 py-2">
-            {review.comment}
-          </p>
-          <div className="flex justify-between my-4">
-            <button className="font-medium flex items-center gap-2">
-              <span>
-                <CornerDownRight size={16} />
-              </span>
-              Reply
-            </button>
-            <div className="flex gap-4">
-              <button
-                onClick={() => handleThumbsUp(review.reviewerEmail)}
-                className="flex items-center gap-2"
-              >
-                <ThumbsUp size={20} strokeWidth={1} />{" "}
-                {reviewState[review.reviewerEmail]?.thumbsUp || 0}
+            <p className="text-gray-600 dark:text-gray-400 py-2">
+              {review.comment}
+            </p>
+            <div className="flex justify-between my-4">
+              <button className="font-medium flex items-center gap-2">
+                <span>
+                  <CornerDownRight size={16} />
+                </span>
+                Reply
               </button>
-              <span className="text-gray-300">|</span>
-              <button
-                onClick={() => handleThumbsDown(review.reviewerEmail)}
-                className="flex items-center gap-2"
-              >
-                <ThumbsDown size={20} strokeWidth={1} />{" "}
-                {reviewState[review.reviewerEmail]?.thumbsDown || 0}
-              </button>
+              <div className="flex gap-4">
+                <button
+                  onClick={() => handleThumbsUp(review.reviewerEmail)}
+                  className="flex items-center gap-2"
+                >
+                  <ThumbsUp size={20} strokeWidth={1} />{" "}
+                  {reviewState[review.reviewerEmail]?.thumbsUp || 0}
+                </button>
+                <span className="text-gray-300">|</span>
+                <button
+                  onClick={() => handleThumbsDown(review.reviewerEmail)}
+                  className="flex items-center gap-2"
+                >
+                  <ThumbsDown size={20} strokeWidth={1} />{" "}
+                  {reviewState[review.reviewerEmail]?.thumbsDown || 0}
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
       <Collapsible open={isOpen} onOpenChange={setIsOpen}>
         <CollapsibleContent className="space-y-2">
-          {sortedReviews.slice(2).map((review, index) => (
+          {reviews.slice(2).map((review, index) => (
             <div key={index} className="p-2 border-b-2 border-gray-200">
               <div className="flex justify-between py-4">
                 <div>

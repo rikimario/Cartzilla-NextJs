a9e350ca91f60fa51e45f392dadf4adbd254ef0d
rikimario <mario_martinov@abv.bg>
2025-04-25 00:50:26 +0300

    add date picker

---
+++ src/app/profile/_components/BasicInfoDatePicker.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
+
+export default function BasicInfoDatePicker() {
+  const [date, setDate] = useState<Date>();
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-full justify-between text-left font-normal px-3",
+            !date && "text-muted-foreground"
+          )}
+        >
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+          <CalendarIcon />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}

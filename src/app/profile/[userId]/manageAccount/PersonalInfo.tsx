c1a96173ea8862d61f2133c14e7aa928e5fe9f46
rikimario <mario_martinov@abv.bg>
2025-04-25 00:50:06 +0300

    create personal info component

--- src/app/profile/[userId]/manageAccount/PersonalInfo.tsx
+++ src/app/profile/[userId]/manageAccount/PersonalInfo.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import BasicInfo from "../../_components/BasicInfo";
 
 export default function PersonalInfo() {
-  return <div>PersonalInfo</div>;
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-gray-700 mb-4 pb-2 dark:text-white">
+        Persianal info
+      </h1>
+
+      <BasicInfo />
+    </>
+  );
 }

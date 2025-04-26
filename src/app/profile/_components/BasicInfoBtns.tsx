2d8c68a671b434cdb945bea1fe787e59834d3988
rikimario <mario_martinov@abv.bg>
2025-04-26 21:25:35 +0300

    add save and close btns

---
+++ src/app/profile/_components/BasicInfoBtns.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function BasicInfoBtns() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mb-8">
+      <Button variant={"destructive"} className="w-full">
+        Save changes
+      </Button>
+      <Button variant={"outline"} className="w-full bg-gray-200">
+        Close
+      </Button>
+    </div>
+  );
+}

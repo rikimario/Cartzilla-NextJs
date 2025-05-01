f514e5daad749890a654d2b6a160e5728e42aae1
rikimario <mario_martinov@abv.bg>
2025-05-01 23:48:18 +0300

    import addNewInfo function

--- src/app/profile/_components/BasicInfoBtns.tsx
+++ src/app/profile/_components/BasicInfoBtns.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
 
-export default function BasicInfoBtns() {
+export default function BasicInfoBtns({
+  addNewInfo,
+}: {
+  addNewInfo: () => void;
+}) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mb-8">
-      <Button variant={"destructive"} className="w-full">
+      <Button onClick={addNewInfo} variant={"destructive"} className="w-full">
         Save changes
       </Button>
       <Button variant={"outline"} className="w-full bg-gray-200">

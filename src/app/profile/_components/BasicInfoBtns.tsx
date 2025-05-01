7604720280ded7509132a903ce1a6d40d8f952ef
rikimario <mario_martinov@abv.bg>
2025-05-02 00:06:12 +0300

    set isOpen to false on close btn

--- src/app/profile/_components/BasicInfoBtns.tsx
+++ src/app/profile/_components/BasicInfoBtns.tsx
@@ -3,15 +3,21 @@ import React from "react";
 
 export default function BasicInfoBtns({
   addNewInfo,
+  setIsOpen,
 }: {
   addNewInfo: () => void;
+  setIsOpen: (isOpen: boolean) => void;
 }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mb-8">
       <Button onClick={addNewInfo} variant={"destructive"} className="w-full">
         Save changes
       </Button>
-      <Button variant={"outline"} className="w-full bg-gray-200">
+      <Button
+        onClick={() => setIsOpen(false)}
+        variant={"outline"}
+        className="w-full bg-gray-200"
+      >
         Close
       </Button>
     </div>

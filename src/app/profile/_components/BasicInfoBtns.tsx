823eb71353da8af6cac13a6da0129c35cc9472ce
rikimario <mario_martinov@abv.bg>
2025-05-06 22:56:37 +0300

    add personal info to check if its empty and disable the close btn

--- src/app/profile/_components/BasicInfoBtns.tsx
+++ src/app/profile/_components/BasicInfoBtns.tsx
@@ -1,20 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { PersonalInformation } from "@/lib/types";
 import React from "react";
 
 export default function BasicInfoBtns({
   addNewInfo,
   setIsOpen,
+  personalInfo,
 }: {
   addNewInfo: () => void;
   setIsOpen: (isOpen: boolean) => void;
+  personalInfo: PersonalInformation | null;
 }) {
+  const isContactInfoEmpty =
+    personalInfo === null || Object.keys(personalInfo).length === 0;
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mb-8">
       <Button onClick={addNewInfo} variant={"destructive"} className="w-full">
         Save changes
       </Button>
       <Button
-        onClick={() => setIsOpen(false)}
+        onClick={() => !isContactInfoEmpty && setIsOpen(false)}
         variant={"outline"}
         className="w-full bg-gray-200"
       >

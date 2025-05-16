4d9595780857b6ff16efd6afe33d0920bf789b9b
rikimario <mario_martinov@abv.bg>
2025-05-16 23:25:50 +0300

    add title for addresses page

--- src/app/profile/[userId]/manageAccount/Addresses.tsx
+++ src/app/profile/[userId]/manageAccount/Addresses.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Addresses() {
-  return <div>Addresses</div>;
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-gray-700 mb-4 pb-2 dark:text-white">
+        Addresses
+      </h1>
+    </div>
+  );
 }

3d1af3008f5310cd91798fdf2b14e66bab289584
rikimario <mario_martinov@abv.bg>
2025-05-22 22:24:23 +0300

    notifications h1

--- src/app/profile/[userId]/manageAccount/Notifications.tsx
+++ src/app/profile/[userId]/manageAccount/Notifications.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Notifications() {
-  return <div>Notifications</div>;
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-gray-700 mb-4 pb-2 dark:text-white">
+        Notifications
+      </h1>
+    </div>
+  );
 }

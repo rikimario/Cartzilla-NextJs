6e7d98e1fdf7f5d5dbdf3419fed09aeb8574413e
rikimario <mario_martinov@abv.bg>
2025-06-04 22:13:38 +0300

    translation

--- src/app/[locale]/profile/_components/OrderProductsSheet.tsx
+++ src/app/[locale]/profile/_components/OrderProductsSheet.tsx
@@ -8,12 +8,14 @@ import {
 } from "@/components/ui/sheet";
 import { Orders } from "@/lib/types";
 import { ChevronRight } from "lucide-react";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import React from "react";
 
 export default function OrderProductsSheet({ order }: { order: Orders }) {
+  const t = useTranslations("Profile");
   return (
-    <div className="">
+    <div>
       <Sheet>
         <SheetTrigger asChild>
           <ul className="ml-4 flex items-center cursor-pointer">
@@ -43,7 +45,7 @@ export default function OrderProductsSheet({ order }: { order: Orders }) {
         <SheetContent className="dark:bg-[#181D25] dark:text-[white] xl:w-[400px]">
           <ScrollArea className="h-full py-4 pr-2">
             <SheetTitle className="text-xl">
-              Order # {order.order_id}
+              {t("order")} # {order.order_id}
             </SheetTitle>
             <div className="my-12">
               {order.products.map((product, index) => (
@@ -61,35 +63,45 @@ export default function OrderProductsSheet({ order }: { order: Orders }) {
                     <p className="text-lg mt-2 font-semibold">
                       ${product.price}
                     </p>
-                    <p className="text-sm mt-2">Qty: {product.quantity}</p>
+                    <p className="text-sm mt-2">
+                      {t("quantity")}: {product.quantity}
+                    </p>
                   </div>
                 </div>
               ))}
               <div className="mt-10 ml-6 py-10 border-y border-gray-200">
-                <h3 className="text-lg font-semibold mb-4">Delivery</h3>
+                <h3 className="text-lg font-semibold mb-4">{t("delivery")}</h3>
                 <div className="flex flex-col gap-4">
                   <div>
                     <p className="text-lg text-gray-500">
-                      Estimated delivery date:
+                      {t("estematedDelivery")}:
                     </p>
                     <span className="text-lg">{order.delivery_date}</span>
                   </div>
 
                   <div>
-                    <p className="text-lg text-gray-500">Shipping method:</p>
-                    <span className="text-lg">Courier delivery</span>
+                    <p className="text-lg text-gray-500">
+                      {t("shippingMethod")}:
+                    </p>
+                    <span className="text-lg">{t("courierDelivery")}</span>
                   </div>
 
                   <div>
-                    <p className="text-lg text-gray-500">Shipping address:</p>
+                    <p className="text-lg text-gray-500">
+                      {t("shippingAddress")}:
+                    </p>
                     <span className="text-lg">{order.address}</span>
                   </div>
                 </div>
 
-                <h3 className="text-lg font-semibold mb-4 pt-8">Payment</h3>
+                <h3 className="text-lg font-semibold mb-4 pt-8">
+                  {t("payment")}
+                </h3>
                 <div className="flex flex-col gap-4">
                   <div>
-                    <p className="text-lg text-gray-500">Payment method:</p>
+                    <p className="text-lg text-gray-500">
+                      {t("paymentMethod")}:
+                    </p>
                     <span className="text-lg">
                       {/\d/.test(order.payment_method)
                         ? `Card: **** **** **** ${order.payment_method.slice(
@@ -100,14 +112,16 @@ export default function OrderProductsSheet({ order }: { order: Orders }) {
                   </div>
 
                   <div>
-                    <p className="text-lg text-gray-500">Shipping:</p>
+                    <p className="text-lg text-gray-500">{t("shipping")}:</p>
                     <span className="text-lg">$16.50</span>
                   </div>
                 </div>
               </div>
 
               <div className="flex justify-between items-center mt-10 ml-6">
-                <h3 className="text-lg text-gray-500">Estimated total:</h3>
+                <h3 className="text-lg text-gray-500">
+                  {t("estimatedTotal")}:
+                </h3>
                 <span className="text-2xl font-semibold mr-6">
                   ${(order.order_total + 16.5).toFixed(2)}
                 </span>
@@ -117,7 +131,7 @@ export default function OrderProductsSheet({ order }: { order: Orders }) {
               variant="outline"
               className="absolute bottom-0 w-full text-lg text-gray-700 bg-gray-200"
             >
-              Change the delivery time
+              {t("changeDelivery")}
             </Button>
           </ScrollArea>
         </SheetContent>

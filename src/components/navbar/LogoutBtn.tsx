b7ccd4bbffa94be3de10c3d6282eb4cc23e3a4c5
rikimario <mario_martinov@abv.bg>
2025-06-04 22:13:47 +0300

    translation

--- src/components/navbar/LogoutBtn.tsx
+++ src/components/navbar/LogoutBtn.tsx
@@ -1,12 +1,14 @@
 import { LogOut } from "lucide-react";
 import { logout } from "../../../utils/supabase/actions";
+import { useTranslations } from "next-intl";
 
 export default function LogoutBtn() {
+  const t = useTranslations("Profile");
   return (
     <>
       <button className="flex gap-2 items-center ml-2" onClick={logout}>
         <LogOut className="h-5 w-5 hidden lg:block" strokeWidth={1} />
-        Logout
+        {t("logout")}
         {/* Logout
         </LogOut> */}
       </button>

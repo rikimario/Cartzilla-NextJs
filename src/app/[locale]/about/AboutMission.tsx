4bb2553341cf7d5304f412c6261954bec3ec4d83
rikimario <mario_martinov@abv.bg>
2025-06-09 00:16:07 +0300

    about mission translation

--- src/app/[locale]/about/AboutMission.tsx
+++ src/app/[locale]/about/AboutMission.tsx
@@ -1,22 +1,23 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import React from "react";
 
 export default function AboutMission() {
+  const t = useTranslations("About");
   return (
     <div className="py-14">
       <div
         id="mission"
         className="flex flex-col justify-center items-center py-14 lg:px-20 xl:px-36"
       >
-        <p className="text-gray-600 dark:text-gray-300">Mission</p>
+        <p className="text-gray-600 dark:text-gray-300">
+          {t("mission.paragraph1")}
+        </p>
         <h1 className="text-2xl md:text-4xl font-semibold text-gray-900 dark:text-white p-6 text-center">
-          The best products at fair prices
+          {t("mission.title")}
         </h1>
         <p className="text-lg text-gray-600 dark:text-gray-300 text-center px-6">
-          "We believe that things exist to make life easier, more pleasant and
-          kinder. That's why the search for the right thing should be quick,
-          convenient and enjoyable. We do not just sell household appliances and
-          electronics, but comfort and accessibility."
+          {t("mission.paragraph2")}
         </p>
         <div className="pt-6">
           <Image

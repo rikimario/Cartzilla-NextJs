62fb8929fa05ad39403c8fea23aa79fbb060d335
rikimario <mario_martinov@abv.bg>
2025-06-09 00:39:51 +0300

    philantropy translation

--- src/app/[locale]/about/Philanthropy.tsx
+++ src/app/[locale]/about/Philanthropy.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import React from "react";
 
 export default function Philanthropy() {
+  const t = useTranslations("About");
   return (
     <div className="py-4 md:flex md:gap-8">
       <div className="py-8 w-full md:w-1/2 md:py-0">
@@ -18,12 +20,10 @@ export default function Philanthropy() {
       <div className="px-4 py-8 md:px-0 bg-gray-100 rounded-xl w-full md:w-1/2 dark:bg-gradient-to-r dark:from-[#1B273A] dark:to-[#1F2632]">
         <div className="p-4 lg:px-8 xl:px-20 h-full flex flex-col justify-center items-start">
           <h1 className="text-2xl xl:text-3xl xl:pr-12 text-gray-900 dark:text-white font-semibold pr-6">
-            The role of philanthropy in building a better world
+            {t("philanthropy.title")}
           </h1>
           <p className="text-gray-600 dark:text-gray-300 py-4">
-            Charitable contributions are a vital aspect of building a better
-            world. These contributions come in various forms, including monetary
-            donations...
+            {t("philanthropy.paragraph")}
           </p>
           <div className="pt-10">
             <Button
@@ -31,7 +31,7 @@ export default function Philanthropy() {
               variant="outline"
               size="lg"
             >
-              Learn more
+              {t("learnMore")}
             </Button>
           </div>
         </div>

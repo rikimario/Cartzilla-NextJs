103d91ce8b3d277653d35c6e95216931d5eccd64
rikimario <mario_martinov@abv.bg>
2025-06-08 23:54:17 +0300

    all categories translation

--- src/app/[locale]/all-categories/page.tsx
+++ src/app/[locale]/all-categories/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import AllCategoriesBrands from "./AllCategoriesBrands";
 import AllCategoriesList from "./AllCategoriesList";
 import AllCategoriesBanner from "./AllCategoriesBanner";
+import { useTranslations } from "next-intl";
 
 export default function page() {
+  const t = useTranslations("AllCategories");
   return (
-    <div className="px-4 md:px-12 lg:px-4 xl:px-32 py-12 dark:bg-[#181D25]">
+    <div className="py-12 dark:bg-[#181D25]">
       <h1 className="text-2xl text-gray-900 dark:text-white font-semibold">
-        Shop Categories
+        {t("title")}
       </h1>
       <AllCategoriesBrands />
       <AllCategoriesList />

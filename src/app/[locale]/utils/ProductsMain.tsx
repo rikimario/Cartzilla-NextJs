1e547c1b59f733a688dd7642fc89697afd07662e
rikimario <mario_martinov@abv.bg>
2025-06-12 23:23:42 +0300

    products main translation

--- src/app/[locale]/utils/ProductsMain.tsx
+++ src/app/[locale]/utils/ProductsMain.tsx
@@ -2,9 +2,11 @@
 import ProductCard from "@/app/[locale]/utils/ProductCard";
 import ProductSkeleton from "@/app/[locale]/utils/ProductSkeleton";
 import { Product } from "@/lib/types";
+import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
 
 export default function ProductsMain({ product }: { product: Product[] }) {
+  const t = useTranslations("Products");
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -15,8 +17,9 @@ export default function ProductsMain({ product }: { product: Product[] }) {
   return (
     <div className="py-10 dark:bg-[#181D25]">
       <p className="text-lg ml-1 text-gray-600 dark:text-gray-300 font-light">
-        Found <span className="font-bold text-gray-700">{product.length}</span>{" "}
-        items
+        {t("productsMain.found")}{" "}
+        <span className="font-bold text-gray-700">{product.length}</span>{" "}
+        {t("productsMain.items")}
       </p>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-4 lg:gap-2 lg:pt-4 items-center justify-center justify-items-center text-center grid-auto-columns">
         {isLoading

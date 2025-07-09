5b4ad04ac6ce09acc0066da525e2e7bc868944a0
rikimario <mario_martinov@abv.bg>
2025-07-10 00:21:28 +0300

    change the redirectTo

--- utils/supabase/actions.ts
+++ utils/supabase/actions.ts
@@ -61,7 +61,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: "https://cartzilla-flame.vercel.app/auth/callback",
+      redirectTo: "https://cartzilla-flame.vercel.app",
       queryParams: {
         access_type: "offline",
         prompt: "consent",

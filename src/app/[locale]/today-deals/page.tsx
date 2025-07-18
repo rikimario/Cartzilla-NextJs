f17d322bef309796617f84ec0424f54adf7bca74
rikimario <mario_martinov@abv.bg>
2025-07-18 11:31:50 +0300

    add today deals page

---
+++ src/app/[locale]/today-deals/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+export default function TodayDeals() {
+  return (
+    <div>
+      <Image
+        className="w-full h-full object-cover"
+        src="/under-construction.webp"
+        alt="under-construction"
+        width={1000}
+        height={1000}
+      />
+    </div>
+  );
+}

56e30a535030ee9f2059f8fc1344025db430c1be
rikimario <mario_martinov@abv.bg>
2025-07-31 23:17:44 +0300

    remove href links

--- src/components/footer/FooterCategories.tsx
+++ src/components/footer/FooterCategories.tsx
@@ -10,7 +10,7 @@ export default function FooterCategories() {
     <div className="text-gray-300 text-sm flex flex-wrap gap-3 pt-6 pb-8">
       {links.map((link) => (
         <span key={link.name}>
-          <Link className="flex gap-2 hover:text-gray-400" href={link.link}>
+          <Link className="flex gap-2 hover:text-gray-400" href="#">
             {link.name} <span className="text-gray-500">/</span>
           </Link>
         </span>
